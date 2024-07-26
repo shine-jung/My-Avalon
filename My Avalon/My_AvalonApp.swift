240db7d1279aa0065d6cabb686bdedaa88df8a4e
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2024-07-26 11:15:21 +0900

    Initial Commit

---
+++ My Avalon/My_AvalonApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_AvalonApp.swift
+//  My Avalon
+//
+//  Created by Seokmin on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct My_AvalonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
